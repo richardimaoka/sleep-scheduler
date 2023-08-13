90ef35f3b45f3d88fe53a4a848ba6c09f3d34906
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-13 21:16:06 +0900

    area chart with error

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,5 +1,14 @@
 "use client";
-import { LineChart, Line, XAxis, YAxis } from "recharts";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  AreaChart,
+  CartesianGrid,
+  Tooltip,
+  Area,
+} from "recharts";
 
 export default function Home() {
   const data = [
@@ -7,14 +16,45 @@ export default function Home() {
     { name: "Page A", uv: 1400, pv: 3400, amt: 5400 },
     { name: "Page A", uv: 1500, pv: 2400, amt: 3400 },
     { name: "Page A", uv: 1800, pv: 3100, amt: 4400 },
+    { name: "Page A", uv: 100, pv: 2100, amt: 4400 },
   ];
   return (
     <main>
-      <LineChart width={400} height={400} data={data}>
-        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
-        <XAxis />
+      <AreaChart
+        width={730}
+        height={250}
+        data={data}
+        margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+      >
+        <defs>
+          <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
+          </linearGradient>
+          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="#82ca9d" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="#82ca9d" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <XAxis dataKey="name" />
         <YAxis />
-      </LineChart>
+        <CartesianGrid strokeDasharray="3 3" />
+        <Tooltip />
+        <Area
+          type="monotone"
+          dataKey="uv"
+          stroke="#8884d8"
+          fillOpacity={1}
+          fill="url(#colorUv)"
+        />
+        <Area
+          type="monotone"
+          dataKey="pv"
+          stroke="#82ca9d"
+          fillOpacity={1}
+          fill="url(#colorPv)"
+        />
+      </AreaChart>
     </main>
   );
 }
