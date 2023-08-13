3c1b1a0125cda308fb701a96784aaed73a53e249
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-13 18:43:49 +0900

    input time

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <main></main>;
+  return (
+    <main>
+      <label htmlFor="from">from</label>
+      <input id="from" type="time" name="from" value="21:00"></input>
+    </main>
+  );
 }
