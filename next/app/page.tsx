5e24be7af35e667653cb4aaed89dba0ef9915938
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-13 21:12:28 +0900

    recharts

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,15 +1,20 @@
+"use client";
+import { LineChart, Line, XAxis, YAxis } from "recharts";
+
 export default function Home() {
+  const data = [
+    { name: "Page A", uv: 400, pv: 2400, amt: 2400 },
+    { name: "Page A", uv: 1400, pv: 3400, amt: 5400 },
+    { name: "Page A", uv: 1500, pv: 2400, amt: 3400 },
+    { name: "Page A", uv: 1800, pv: 3100, amt: 4400 },
+  ];
   return (
     <main>
-      <label htmlFor="from">from</label>
-      <input id="from" type="time" name="from" value="21:00"></input>
-      <label htmlFor="fromdate">from</label>
-      <input
-        id="fromdate"
-        type="date"
-        name="fromdate"
-        value="2023/7/20"
-      ></input>
+      <LineChart width={400} height={400} data={data}>
+        <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+        <XAxis />
+        <YAxis />
+      </LineChart>
     </main>
   );
 }
