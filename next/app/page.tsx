bbe78b5fca22b73450cb6d9b51d281e70ac997c9
Richard Imaoka <richard.e.imaoka@gmail.com>
2023-08-13 18:46:12 +0900

    input date

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -3,6 +3,13 @@ export default function Home() {
     <main>
       <label htmlFor="from">from</label>
       <input id="from" type="time" name="from" value="21:00"></input>
+      <label htmlFor="fromdate">from</label>
+      <input
+        id="fromdate"
+        type="date"
+        name="fromdate"
+        value="2023/7/20"
+      ></input>
     </main>
   );
 }
